e180518c664785edb07471cb23eceda58c59fab1
MatheusTrigoni <matheusbr360@gmail.com>
2024-05-06 00:47:59 -0300

    Integração com banco de dados junto com verificações de login e cadastro e finalização da estruturação da tela Home

--- lib/home.dart
+++ lib/home.dart
@@ -1,46 +1,161 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
+  static const Color cinzaEscuro = Color(0xFF2F2F2F);
+  static const Color brancoAcizentado = Color(0xFFEAEAEA);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        flexibleSpace: const Image(
-          image: AssetImage('images/fundo-app-bar.png'),
-          fit: BoxFit.cover,
-        ),
-        elevation: 0,
-        automaticallyImplyLeading: false,
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/mapa-temp.png'),
-            fit: BoxFit.cover
-          )
-        ),
-        child: Container(
-          alignment: Alignment.topCenter,
-          margin: const EdgeInsets.fromLTRB(20, 32, 20, 0),
-          child: TextField(
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: const Color(0xFF2F2F2F),
-              labelText: 'Endereço, Shopping...',
-              labelStyle: const TextStyle(
-                fontFamily: 'Raleway',
-                fontSize: 16,
-                color: Color(0xFF898989)
-              ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(24)
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: MediaQuery.of(context).size.height * 0.089,
+          flexibleSpace: const Image(
+            image: AssetImage('images/fundo-app-bar.png'),
+            fit: BoxFit.cover,
+          ),
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  print('BOTÃO SINO PRESSIONADO');
+                },
+                iconSize: 48,
+                color: Colors.white,
+                icon: const ImageIcon(
+                  AssetImage('images/sino.png')
+                )
+            ),
+            Container( // Deveria acrescentar um Container no botão anterior?
+              margin: const EdgeInsets.fromLTRB(4, 0, 32, 0),
+              child: IconButton(
+                  onPressed: () {
+                    print('BOTÃO USUÁRIO PRESSIONADO');
+                  },
+                  color: Colors.black,
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.white),
+                    padding: MaterialStatePropertyAll(EdgeInsets.zero)
+                  ),
+                  iconSize: 48,
+                  icon: const ImageIcon(
+                      AssetImage('images/user.png')
+                  )
               ),
             ),
+          ],
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/mapa-temp.png'),
+              fit: BoxFit.cover
+            )
+          ),
+          child: Container(
+            alignment: Alignment.topCenter,
+            margin: const EdgeInsets.fromLTRB(20, 32, 20, 0),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: cinzaEscuro,
+                prefixIcon: const Icon(Icons.search, color: brancoAcizentado),
+                hintText: 'Endereço, Shopping...',
+                hintStyle: const TextStyle(
+                  fontFamily: 'Raleway',
+                  fontSize: 16,
+                  color: Color(0xFF898989),
+                  fontWeight: FontWeight.normal
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(24)
+                ),
+              ),
+            ),
+          ),
+        ),
+        bottomNavigationBar: Container(
+          padding: const EdgeInsets.fromLTRB(0, 23, 0, 0),
+          height: MediaQuery.of(context).size.height * 0.122,
+          decoration: const BoxDecoration(
+            color: cinzaEscuro,
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(24), topRight: Radius.circular(24))
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      print('BOTÃO CARTEIRA PRESSIONADO');
+                    },
+                    icon: const ImageIcon(
+                      AssetImage('images/carteira.png')
+                    ),
+                    color: brancoAcizentado,
+                  ),
+                  const Text(
+                    'Carteira',
+                    style: TextStyle(
+                      color: brancoAcizentado,
+                      fontFamily: 'Raleway',
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        print('BOTÃO CARTEIRA PRESSIONADO');
+                      },
+                      icon: const ImageIcon(
+                          AssetImage('images/qr-code.png')
+                      ),
+                      color: brancoAcizentado
+                  ),
+                  const Text(
+                    'Pagar',
+                    style: TextStyle(
+                        color: brancoAcizentado,
+                        fontFamily: 'Raleway',
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        print('BOTÃO CARTEIRA PRESSIONADO');
+                      },
+                      icon: const ImageIcon(
+                          AssetImage('images/bateria.png')
+                      ),
+                      color: brancoAcizentado
+                  ),
+                  const Text(
+                    'Carregar',
+                    style: TextStyle(
+                        color: brancoAcizentado,
+                        fontFamily: 'Raleway',
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal
+                    ),
+                  )
+                ],
+              ),
+            ],
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,140 +1,315 @@
-import 'dart:ui';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
+import 'package:voltz/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'home.dart';
 
-void main() => runApp(const MaterialApp(
-  home: VoltzHome()
-));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class VoltzHome extends StatelessWidget {
-  const VoltzHome({super.key});
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(MaterialApp(
+    home: VoltzMain()
+  ));
+}
+
+class VoltzMain extends StatefulWidget {
+  VoltzMain({super.key});
+
+  static const Color cinzaVoltz = Color(0xFFA4A4A4);
+  static const Color azulVoltz = Color(0xFF2F88CC);
+  static const Color brancoAcizentado = Color(0xFFEAEAEA);
+
+  @override
+  State<VoltzMain> createState() => _VoltzMainState();
+}
+
+class _VoltzMainState extends State<VoltzMain> {
+  TextEditingController controladorEmail = TextEditingController();
+  TextEditingController controladorSenha = TextEditingController();
+
+  bool usuarioExiste = false;
+
+  Future<void> criarConta(String email, String senha) async {
+    if (email == '' || senha == '') {
+      usuarioExiste = false;
+      print('Falha ao criar conta, um ou mais campos estão vazios');
+      return;
+    }
+
+    try {
+      QuerySnapshot consulta = await FirebaseFirestore.instance
+      .collection('usuarios')
+      .where('email', isEqualTo: email)
+      .get();
+
+      if (consulta.size == 0) {
+        await FirebaseFirestore.instance.collection('usuarios').add({
+          'email': email,
+          'senha': senha,
+        });
+
+        usuarioExiste = true;
+        print('Usuário criado com sucesso');
+      } else {
+        usuarioExiste = false;
+        print('Falha ao criar conta, conta já existente');
+      }
+    } catch (e) {
+      print('Erro ao criar conta: $e');
+    }
+  }
+
+  Future<void> efetuarLogin(String email, String senha) async {
+    if (email == '' || senha == '') {
+      usuarioExiste = false;
+      print('Falha ao efetuar login, um ou mais campos estão vazios');
+      return;
+    }
+
+    try {
+      QuerySnapshot consulta = await FirebaseFirestore.instance
+      .collection('usuarios')
+      .where('email', isEqualTo: email)
+      .where('senha', isEqualTo: senha)
+      .get();
+
+      if (consulta.size == 0) {
+        usuarioExiste = false;
+        print('O email e(ou) a senha estão errados ou o usuário não existe');
+      } else {
+        usuarioExiste = true;
+        print('Login efetuado com sucesso');
+      }
+    } catch (e) {
+      print('Erro ao efetuar login: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFEAEAEA),
+      backgroundColor: VoltzMain.brancoAcizentado,
       appBar: null,
-      body: Column(
-        children: [
-          const Expanded(
-            flex: 2,
-            child: Image(
-              image: AssetImage(
-                'images/fundo.png',
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.65,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'images/fundo.png',
+                    ),
+                    fit: BoxFit.cover,
+                  )
               ),
-              fit: BoxFit.cover,
-            )
-          ),
-          Expanded(
-            flex: 1,
-            child: Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.fromLTRB(20, 6, 20, 6),
-                  child: const TextField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Color(0xFFA4A4A4),
-                            )
-                        ),
-                        labelText: 'Email ou CPF',
-                        labelStyle: TextStyle(
-                            color: Color(0xFFA4A4A4),
-                            fontFamily: 'Raleway',
-                            fontSize: 20
-                        )
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(20, 1, 20, 2),
-                  child: const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color(0xFFA4A4A4),
-                        ),
-                      ),
-                      labelText: 'Senha',
-                      labelStyle: TextStyle(
-                          color: Color(0xFFA4A4A4),
-                          fontFamily: 'Raleway',
-                          fontSize: 20
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(192, 3, 0, 4),
-                  child: const Text(
-                    'Esqueceu a senha? Clique aqui',
-                    style: TextStyle(
-                      color: Color(0xFF2F88CC),
-                      fontSize: 12,
-                      fontFamily: 'Raleway',
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.fromLTRB(106, 10, 106, 10),
-                  margin: const EdgeInsets.fromLTRB(0, 4, 0, 6),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          color: const Color(0xFF2F88CC),
-                          width: 2
-                      ),
-                      borderRadius: BorderRadius.circular(4)
-                  ),
-                  child: const Text(
-                    'Criar uma conta',
-                    style: TextStyle(
-                        color: Color(0xFF2F88CC),
-                        fontSize: 20,
-                        fontFamily: 'Raleway',
-                        fontWeight: FontWeight.bold
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const Home())
-                    );
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.fromLTRB(156, 10, 156, 10),
-                    margin: const EdgeInsets.fromLTRB(0, 4, 0, 6),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(4),
-                        color: const Color(0xFF2F88CC)
-                    ),
-                    child: const Text(
-                      'Entrar',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontFamily: 'Raleway',
-                          fontWeight: FontWeight.bold
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-              ],
             ),
-          )
-        ],
+            Container(
+              margin: const EdgeInsets.fromLTRB(20, 6, 20, 6),
+              child: TextField(
+                controller: controladorEmail,
+                style: const TextStyle(
+                  color: VoltzMain.azulVoltz,
+                  fontFamily: 'Raleway',
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal
+                ),
+                autocorrect: false,
+                cursorColor: VoltzMain.azulVoltz,
+                decoration: const InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: VoltzMain.azulVoltz,
+                      width: 2
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.all(13.5),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: VoltzMain.cinzaVoltz,
+                    ),
+                  ),
+                  hintText: 'Email',
+                  hintStyle: TextStyle(
+                    color: VoltzMain.cinzaVoltz,
+                    fontFamily: 'Raleway',
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal
+                  )
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(20, 1, 20, 2),
+              child: TextField(
+                controller: controladorSenha,
+                style: const TextStyle(
+                    color: VoltzMain.azulVoltz,
+                    fontFamily: 'Raleway',
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal
+                ),
+                cursorColor: VoltzMain.azulVoltz,
+                autocorrect: false,
+                enableSuggestions: false,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: VoltzMain.azulVoltz,
+                        width: 2
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.all(13.5),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: VoltzMain.cinzaVoltz,
+                    ),
+                  ),
+                  hintText: 'Senha',
+                  hintStyle: TextStyle(
+                      color: VoltzMain.cinzaVoltz,
+                      fontFamily: 'Raleway',
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(192, 3, 0, 4),
+              child: const Text(
+                'Esqueceu a senha? Clique aqui',
+                style: TextStyle(
+                  color: VoltzMain.azulVoltz,
+                  fontSize: 12,
+                  fontFamily: 'Raleway',
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                criarConta(controladorEmail.text, controladorSenha.text).then((_) {
+                  if (usuarioExiste) {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const Home()));
+                  } else {
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        backgroundColor: VoltzMain.brancoAcizentado,
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text(
+                              'Fechar',
+                              style: TextStyle(
+                                color: VoltzMain.azulVoltz,
+                                fontFamily: 'Raleway',
+                                fontWeight: FontWeight.bold
+                              ),
+                            )
+                          )
+                        ],
+                        title: const Text(
+                          'Erro ao criar conta',
+                          style: TextStyle(fontFamily: 'Raleway', fontWeight: FontWeight.bold)
+                        ),
+                        content: const Text(
+                          'Você tentou criar uma conta com um email já registrado ou um ou mais campos não foram preenchidos.',
+                          style: TextStyle(fontFamily: 'Raleway', fontWeight: FontWeight.normal)
+                        )
+                      )
+                    );
+                  }
+                });
+              },
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(106, 10, 106, 10),
+                margin: const EdgeInsets.fromLTRB(0, 4, 0, 6),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                        color: VoltzMain.azulVoltz,
+                        width: 2
+                    ),
+                    borderRadius: BorderRadius.circular(4)
+                ),
+                child: const Text(
+                  'Criar uma conta',
+                  style: TextStyle(
+                      color: VoltzMain.azulVoltz,
+                      fontSize: 20,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.bold
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                efetuarLogin(controladorEmail.text, controladorSenha.text).then((_) {
+                  if (usuarioExiste) {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const Home()));
+                  } else {
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        backgroundColor: VoltzMain.brancoAcizentado,
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text(
+                              'Fechar',
+                              style: TextStyle(
+                                color: VoltzMain.azulVoltz,
+                                fontFamily: 'Raleway',
+                                fontWeight: FontWeight.bold
+                              ),
+                            )
+                          )
+                        ],
+                        title: const Text(
+                          'Erro ao efetuar login',
+                          style: TextStyle(fontFamily: 'Raleway', fontWeight: FontWeight.bold)
+                        ),
+                        content: const Text(
+                          'O email e(ou) a senha estão errados, o usuário não existe ou um ou mais campos não foram preenchidos.',
+                          style: TextStyle(fontFamily: 'Raleway', fontWeight: FontWeight.normal)
+                        )
+                      )
+                    );
+                  }
+                });
+              },
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(156, 10, 156, 10),
+                margin: const EdgeInsets.fromLTRB(0, 4, 0, 6),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: VoltzMain.azulVoltz
+                ),
+                child: const Text(
+                  'Entrar',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.bold
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
